4575c7fd4c42a901eba3ae497b34ef6a66fb48d5
Rishabh anand <90849358+rishabhanand2@users.noreply.github.com>
2021-10-21 12:34:09 +0530

    Update bot_alive.py

--- userbot/modules/assistant/bot_alive.py
+++ userbot/modules/assistant/bot_alive.py
@@ -5,9 +5,9 @@ from userbot import ALIVE_NAME, bot
 currentversion = "1.0"
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Javes"
-PM_IMG = "https://telegra.ph/file/2f2b8d40e3f2fa4acdc8f.mp4"
-pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "lucybot"
+PM_IMG = "https://telegra.ph/file/1aa05b6f356b259e7a0d6.jpg"
+pm_caption = "➥ **LUCY IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
 pm_caption += "➥ **Python:** `3.7.4` \n"
@@ -17,8 +17,8 @@ pm_caption += f"➥ **Version** : `{currentversion}`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
 pm_caption += "➥ **License** : [GNU General Public License v3.0](https://github.com/KeinShin/Black-Lightning/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [Javes](GitHub.com/Javes786)\n"
-pm_caption += "[Assistant By Javes 🇮🇳](https://telegra.ph/file/2f2b8d40e3f2fa4acdc8f.mp4)"
+pm_caption += "➥ **Copyright** : By [lucy](GitHub.com/rishabhanand2)\n"
+pm_caption += "[Assistant By LUCY 🇮🇳](https://telegra.ph/file/1aa05b6f356b259e7a0d6.jpg)"
 
 
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
